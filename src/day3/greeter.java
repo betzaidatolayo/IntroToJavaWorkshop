d26d39d2663394e7fe581ed88c805cafa19a88c2
betzaidatolayo <league@Mac-mini-2.local>
2017-03-29 12:01:59 -0700

    we made the comport to speck

---
+++ src/day3/greeter.java
@@ -0,0 +1,19 @@
+package day3;
+
+import javax.swing.JApplet;
+import javax.swing.JOptionPane;
+
+public class greeter {
+	public static void main(String[] args) {
+System.out.println("early bird get a worm");	
+JOptionPane.showMessageDialog(null,"hi");
+JOptionPane.showMessageDialog(null, "hi there");
+JOptionPane.showMessageDialog(null, "lts go play");
+JOptionPane.showMessageDialog(null, "nice glasses");
+JOptionPane.showMessageDialog(null,"hello is it sunny" );
+	}
+	
+	
+	
+	
+}
---
+++ src/day3/snooper.java
@@ -0,0 +1,55 @@
+package day3;
+
+import javax.swing.JOptionPane;
+
+public class snooper {public static void main(String[] args) {
+String name =JOptionPane.showInputDialog("what is your name?");	
+JOptionPane.showMessageDialog(null,"hi "+name);
+String house =JOptionPane.showInputDialog(" where do you live");
+String animals=JOptionPane.showInputDialog("what is you fav animal");
+String color=JOptionPane.showInputDialog("what you fav color");
+string song=JOptionPane.showInputDialog("what you fav song");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
